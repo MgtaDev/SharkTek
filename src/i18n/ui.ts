1cfba3ef261a683537920b0e3f5bd2434a054c2a
Daniel Passantino <passantinodev@gmail.com>
2024-10-08 20:51:33 -0400

    changes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,21 +14,21 @@ export const languages = {
       'footer.services': 'Services',
       'footer.about': 'About',
       'footer.slogan':'We exponentially grow your project',
-      'footer.conctact-title': 'Conctact',
+      'footer.conctact-title': 'Contact',
       'footer.subscribe-title': 'Subscribe',
-      'footer.rights':'© 2023 C² Developer Group - All rights reserved',
-      'footer.developed-by' : 'Developed by C² Developer Group',
+      'footer.rights':'© 2023 Sharktek - All rights reserved',
+      'footer.developed-by' : 'Developed by SharkTek',
       'footer.newsletter-placeholder': 'Newsletter',
       // Reviews
-      'reviews.review1': 'We needed a website to promote our computer assistance and optimization technical services, C² Developer Group built the page from scratch and since then we have received a positive feedback from customers since presenting a page of this level of quality generates a engagement and catches the attention of people allowing you to see the type of work we do, our servivios, etc.',
+      'reviews.review1': 'We needed a website to promote our computer assistance and optimization technical services, Sharktek built the page from scratch and since then we have received a positive feedback from customers since presenting a page of this level of quality generates a engagement and catches the attention of people allowing you to see the type of work we do, our servivios, etc.',
 
-      'reviews.review2': 'In beingØ we wanted to develop an app website for the management of data and procreo to a freelance, organization or company that had an immense information to organize and analyze in the long term in order to see the results of its progress and development. C² Developer Group helped us optimize our system, fix bugs and prepare exceptional undesiño for the comfort of our users, we are fully grateful for their professionalism and good treatment with our team.',
+      'reviews.review2': 'In beingØ we wanted to develop an app website for the management of data and procreo to a freelance, organization or company that had an immense information to organize and analyze in the long term in order to see the results of its progress and development. Sharktekhelped us optimize our system, fix bugs and prepare exceptional undesiño for the comfort of our users, we are fully grateful for their professionalism and good treatment with our team.',
 
-      'reviews.review3': 'We hire C² Developer Group to redesign the website and give it a fresh and updated air that will cause a good impression, since then we have experienced a 41% increase in our traffic and a 22% increase in the amount of conversions. We recommend them to all companies seeking to improve their presence online.',
+      'reviews.review3': 'We hire Sharktekto redesign the website and give it a fresh and updated air that will cause a good impression, since then we have experienced a 41% increase in our traffic and a 22% increase in the amount of conversions. We recommend them to all companies seeking to improve their presence online.',
 
-      'reviews.review4': 'Thanks to C² Developer Group we have an excellent online store with high client traffic that buy our portfolio daily, we have received a great feedback for the proper functioning of the system that developed us and we recommend them to any company or brand that is starting in the Product sales market, it really generates great business progress and optimizes many tasks with our customers.',
+      'reviews.review4': 'Thanks to Sharktekwe have an excellent online store with high client traffic that buy our portfolio daily, we have received a great feedback for the proper functioning of the system that developed us and we recommend them to any company or brand that is starting in the Product sales market, it really generates great business progress and optimizes many tasks with our customers.',
 
-      'reviews.review5': 'Thanks to C² Developer Group we have an excellent online store with high client traffic that buy our portfolio daily, we have received a great feedback for the proper functioning of the system that developed us and we recommend them to any company or brand that is starting in the Product sales market, it really generates great business progress and optimizes many tasks with our customers.',
+      'reviews.review5': 'Thanks to Sharktekwe have an excellent online store with high client traffic that buy our portfolio daily, we have received a great feedback for the proper functioning of the system that developed us and we recommend them to any company or brand that is starting in the Product sales market, it really generates great business progress and optimizes many tasks with our customers.',
 
       // What we Do
       'ww2.Ecommerce' : 'E-commerce',
@@ -62,24 +62,24 @@ export const languages = {
       'footer.slogan':'Exponenciamos tu proyecto',
       'footer.conctact-title': 'Conctacto',
       'footer.subscribe-title': 'Subscríbete',
-      'footer.rights':'© 2023 C² Developer Group - Todos los derechos reservados',
-      'footer.developed-by' : 'Desarrollado por C² Developer Group',
+      'footer.rights':'© 2023 Sharktek - Todos los derechos reservados',
+      'footer.developed-by' : 'Desarrollado por Sharktek',
       'footer.newsletter-placeholder': 'Boletín Informatívo',
       
       // Review 1
-      'reviews.review1': 'Necesitabamos un sitio web para promocionar nuestros servicios técnicos de asistencia y optimización de computadoras, C² Developer Group nos construyó la página desde cero y desde entonces hemos recibido un feedback positivo por parte de los clientes ya que presentar una página de este nivel de calidad genera un engagement y atrapa la atención de las personas permitiendo que pueden ver el tipo de trabajo que hacemos, nuestros servivios, etc.',
+      'reviews.review1': 'Necesitabamos un sitio web para promocionar nuestros servicios técnicos de asistencia y optimización de computadoras, Sharkteknos construyó la página desde cero y desde entonces hemos recibido un feedback positivo por parte de los clientes ya que presentar una página de este nivel de calidad genera un engagement y atrapa la atención de las personas permitiendo que pueden ver el tipo de trabajo que hacemos, nuestros servivios, etc.',
 
       // Review2
-      'reviews.review2': 'En SERØ queríamos desarrollar una web app para el manejo de datos yprogreso de un freelance, organización o empresa que tuviera una inmensacantidad de datos que organizar y analizar a largo plazo con el fin de ver los resultados de su progreso y desarrollo. C² Developer Group nos ayudó a optimizar nuestro sistema, arreglar bugs y elaborar undiseño excepcional para la comodidad de nuestros usuarios, estamoscompletamente agradecidos por su profesionalidad y buen trato con nuestro equipo.',
+      'reviews.review2': 'En SERØ queríamos desarrollar una web app para el manejo de datos yprogreso de un freelance, organización o empresa que tuviera una inmensacantidad de datos que organizar y analizar a largo plazo con el fin de ver los resultados de su progreso y desarrollo. Sharkteknos ayudó a optimizar nuestro sistema, arreglar bugs y elaborar undiseño excepcional para la comodidad de nuestros usuarios, estamoscompletamente agradecidos por su profesionalidad y buen trato con nuestro equipo.',
 
       // Review 3
-      'reviews.review3': 'Contratamos a C² Developer Group para rediseñar la página web y darle un aire fresco y actualizado que causará una buena impresión, desde entonces hemos experimentado un aumento del 41% en nuestro tráfico y un aumento de 22% en la cantidad de conversiones. Los recomendamos a todas las compañías buscando mejorar su presencia en línea.',
+      'reviews.review3': 'Contratamos a Sharktekpara rediseñar la página web y darle un aire fresco y actualizado que causará una buena impresión, desde entonces hemos experimentado un aumento del 41% en nuestro tráfico y un aumento de 22% en la cantidad de conversiones. Los recomendamos a todas las compañías buscando mejorar su presencia en línea.',
 
       // Review 4
-      'reviews.review4': 'Gracias a C² Developer Group tenemos una excelente tienda online con un alto tráfico de clientes que compran a diario nuestras carteras, hemos recibido un gran feedback por el buen funcionamiento del sistema que nos desarrollaron y los recomendamos a cualquier empresa o marca que está comenzando en el mercado de venta de productos, realmente genera un gran progreso empresarial y optimiza muchas tareas con nuestros clientes.',
+      'reviews.review4': 'Gracias a Sharktektenemos una excelente tienda online con un alto tráfico de clientes que compran a diario nuestras carteras, hemos recibido un gran feedback por el buen funcionamiento del sistema que nos desarrollaron y los recomendamos a cualquier empresa o marca que está comenzando en el mercado de venta de productos, realmente genera un gran progreso empresarial y optimiza muchas tareas con nuestros clientes.',
 
          // Review 5
-      'reviews.review5': 'Gracias a C² Developer Group tenemos una excelente tienda online con un alto tráfico de clientes que compran a diario nuestras carteras, hemos recibido un gran feedback por el buen funcionamiento del sistema que nos desarrollaron y los recomendamos a cualquier empresa o marca que está comenzando en el mercado de venta de productos, realmente genera un gran progreso empresarial y optimiza muchas tareas con nuestros clientes.',
+      'reviews.review5': 'Gracias a Sharktektenemos una excelente tienda online con un alto tráfico de clientes que compran a diario nuestras carteras, hemos recibido un gran feedback por el buen funcionamiento del sistema que nos desarrollaron y los recomendamos a cualquier empresa o marca que está comenzando en el mercado de venta de productos, realmente genera un gran progreso empresarial y optimiza muchas tareas con nuestros clientes.',
 
       // What we Do
       'ww2.Ecommerce' : 'E-commerce',
